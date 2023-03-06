4d96569bb7a6a1dde63658aedf1e83db8167648f
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-03-06 09:44:53 +0100

    added some documentation

--- utils/redis.js
+++ utils/redis.js
@@ -1,6 +1,13 @@
 import { createClient } from 'redis';
 
+/** This file contains functions that help work
+  * with the redis client
+  */
+
 class RedisClient {
+/**
+  * Create a new redis client instance
+  */
   constructor() {
     this.client = createClient();
     this.client.connect();
@@ -14,11 +21,23 @@ class RedisClient {
     });
   }
 
+  /**
+   * Gets the value of a given key.
+   * @param {STRING} key The key of the item to get.
+   * @returns {Srtring | Object}
+   */
   async get(key) {
     const value = await this.client.get(key);
     return value;
   }
 
+  /**
+   *
+   * @param {STRING} key the key of the value to be set.
+   * @param {STRING} value the values to be stored.
+   * @param {STRING | Number | Boolean} duration time before the cache expires.
+   * @returns {Promise<void>}
+   */
   async set(key, value, duration) {
     return this.client.set(key, value, { EX: duration });
   }
