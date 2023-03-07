deb86728d0a15a2576d35cc981af0d94c9dbc311
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-03-07 22:17:41 +0100

    added more routes to the index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 export default function Routes(app) {
   const router = express.Router();
@@ -11,5 +13,21 @@ export default function Routes(app) {
 
   router.get('/stats', (req, res) => {
     AppController.getStats(req, res);
+  });
+
+  router.post('/users', (req, res) => {
+    UsersController.postNew(req, res);
+  });
+
+  router.get('/connect', (req, res) => {
+    AuthController.getConnect(req, res);
+  });
+
+  router.get('/disconnect', (req, res) => {
+    AuthController.getDisconnect(req, res);
+  });
+
+  router.get('/users/me', (req, res) => {
+    UsersController.getMe(req, res);
   });
 }
