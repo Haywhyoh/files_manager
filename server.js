8a502a41d3419f0caf9e5456cd279f38900d564f
Adedayo Ayomide <adefeyisayo998@gmail.com>
2023-03-07 22:16:36 +0100

    rearrange the order of the middle wares

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 import express from 'express';
 import Routes from './routes/index';
 
-
 const app = express();
-Routes(app)
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+Routes(app);
 
-app.listen(port, ()=> {
+app.listen(port, () => {
   console.log(`App is running on port ${port}`);
 });
 
